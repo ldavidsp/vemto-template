f9891a7a0d6a13cd22c8b4f8eff86d22437d0b54
GitHub Action <action@github.com>
2022-04-22 00:05:37 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "vemto-template"
--- src/main/kotlin/com/github/ldavidsp/vemtotemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ldavidsp/vemtotemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ldavidsp.vemtotemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ldavidsp.vemtotemplate.MyBundle
 
 class MyApplicationService {
 
